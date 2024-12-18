7986ef1fedb23cdf0d23f8c10d8c24c49d4b533b
CBurge95 <74315075+CBurge95@users.noreply.github.com>
2024-12-18 13:50:02 +0000

    Update create.py

--- src/kiara_plugin/tropy/modules/create.py
+++ src/kiara_plugin/tropy/modules/create.py
@@ -270,9 +270,9 @@ class AssembleGraphFromTablesModule(KiaraModule):
             
             if graph_type_str == 'undirected':
                 table = (edges_table.arrow_table).select([edges_source_column_name, edges_target_column_name])
-                edges = [(item[0], item[1]) for item in [list(items.values()) for items in table.to_pylist()]]
-                edge_list = set(edges)
-                for item in edge_list:
+                edges = set([(item[0], item[1]) for item in [list(items.values()) for items in table.to_pylist()]])
+                edge_list = []
+                for item in edges:
                     if (item[0], item[1]) not in edge_list:
                         if (item[1], item[0]) not in edge_list:
                             edge_list.append([item[0], item[1]])
