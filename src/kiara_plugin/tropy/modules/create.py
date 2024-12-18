b1b81378091b5beaddfa9316a85707732ac4095c
CBurge95 <74315075+CBurge95@users.noreply.github.com>
2024-12-18 14:10:55 +0000

    Update create.py

--- src/kiara_plugin/tropy/modules/create.py
+++ src/kiara_plugin/tropy/modules/create.py
@@ -262,8 +262,8 @@ class AssembleGraphFromTablesModule(KiaraModule):
         if is_weighted == False:
             if graph_type_str == 'directed':
                 table = (edges_table.arrow_table).select([edges_source_column_name, edges_target_column_name])
-                edges = [(item[0], item[1]) for item in [list(items.values()) for items in table.to_pylist()]]
-                edges_table_data = [[item[0] for item in set(edges)], [item[1] for item in set(edges)]]
+                edges = set([(item[0], item[1]) for item in [list(items.values()) for items in table.to_pylist()]])
+                edges_table_data = [[item[0] for item in edges], [item[1] for item in edges]]
                 data_arrays = [pa.array(col) for col in edges_table_data]
                 column_names = [edges_source_column_name, edges_target_column_name]
                 edges_table = pa.Table.from_arrays(data_arrays, names=column_names)
@@ -273,8 +273,8 @@ class AssembleGraphFromTablesModule(KiaraModule):
                 edges = set([(item[0], item[1]) for item in [list(items.values()) for items in table.to_pylist()]])
                 edge_list = []
                 for item in edges:
-                    if [item[0],item[1]] not in edge_list:
-                        edge_list.append([item[1],item[0]])
+                    if [item[1],item[0]] not in edge_list:
+                        edge_list.append([item[0],item[1]])
                 edges_table_data = [[item[0] for item in edge_list], [item[1] for item in edge_list]]
                 data_arrays = [pa.array(col) for col in edges_table_data]
                 column_names = [edges_source_column_name, edges_target_column_name]
