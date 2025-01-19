c11e04cff2b339fd869f993e2afdf3089fa9ecf7
CBurge95 <74315075+CBurge95@users.noreply.github.com>
2025-01-19 16:09:59 +0000

    Update job_log.py

--- src/kiara_plugin/tropy/modules/job_log.py
+++ src/kiara_plugin/tropy/modules/job_log.py
@@ -110,7 +110,7 @@ class JobLog(KiaraModule):
                             else:
                                 _outputs = (_outputs + (f"\n{name}: \n {kiara._api.render_value(value=id, target_format="string").rendered[:max_char]}"))
                         else:
-                                _outputs = (_outputs + (f"\n{name}: \n {kiara._api.render_value(value=id, target_format="string").rendered[:max_char]}"))
+                             _outputs = (_outputs + (f"\n{name}: \n {kiara._api.render_value(value=id, target_format="string").rendered[:max_char]}"))
                 _job_log = (_job_log + _outputs)
                 JOB_LOG = (JOB_LOG + _job_log)
                 if export == 'csv':
