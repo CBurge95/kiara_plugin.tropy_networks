17f9605bcc8ee069d839ebdbf6bcdf43245b3e27
CBurge95 <74315075+CBurge95@users.noreply.github.com>
2025-01-19 15:49:30 +0000

    Update job_log.py

--- src/kiara_plugin/tropy/modules/job_log.py
+++ src/kiara_plugin/tropy/modules/job_log.py
@@ -65,8 +65,9 @@ class JobLog(KiaraModule):
         export = inputs.get_value_obj('export_type')
 
         EXPORT_TYPES = ['txt', 'csv']
-        if export not in EXPORT_TYPES:
-             raise KiaraProcessingException('Export type not supported. Please select "txt" or "csv"')
+        if export != None:
+            if export not in EXPORT_TYPES:
+                raise KiaraProcessingException('Export type not supported. Please select "txt" or "csv"')
         
         if export == 'csv':
             job_table = pd.DataFrame(columns=['Module Name', 'Comments', 'Time Submitted', 'Runtime', 'Inputs', 'Outputs'])
