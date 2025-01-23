4e67b7d8d47ae6452fe3a22255cf4479e743c3de
CBurge95 <74315075+CBurge95@users.noreply.github.com>
2025-01-23 16:45:16 +0000

    Update preview_graph.py

--- src/kiara_plugin/tropy/modules/preview_graph.py
+++ src/kiara_plugin/tropy/modules/preview_graph.py
@@ -126,6 +126,6 @@ class Preview_Network_Info(KiaraModule):
             info = (info + '\n \nYou have more edges in a directed graph than in an undirected graph. \nThis means you have reciprocal edges between at least one pair of nodes. \nIf this doesn\'t sound correct for your datatype, please recheck your data.')
 
         if DIRECTED_MULTI.number_of_edges() > DIRECTED.number_of_edges():
-            info = (info + '\n \nYou have more edges in a multi graph than in an directed or undirected graph. \nThis means you have parallel edges between at least one pair of nodes. For more options on how to handle parallel edges, see the \'assemble.network_graph\' module. \nIf this doesn\'t sound correct for your datatype, please recheck your data.')
+            info = (info + '\n \nYou have more edges in a multi graph than in an undirected or directed graph. \nThis means you have parallel edges between at least one pair of nodes. For more options on how to handle parallel edges, see the \'assemble.network_graph\' module. \nIf this doesn\'t sound correct for your datatype, please recheck your data.')
 
         outputs.set_values(preview=info)
