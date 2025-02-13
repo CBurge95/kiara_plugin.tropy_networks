bbc2cc8b30581ec1894c5dc76909b7e05cbc57af
CBurge95 <74315075+CBurge95@users.noreply.github.com>
2025-02-13 09:08:44 +0000

    Update paths.py

--- src/kiara_plugin/tropy/modules/paths.py
+++ src/kiara_plugin/tropy/modules/paths.py
@@ -55,3 +55,50 @@ class AveragePath(KiaraModule):
         path = nx.average_shortest_path_length(G.to_undirected(), weight=weights)
 
         outputs.set_values(path=path)
+
+
+
+class Diameter(KiaraModule):
+    """Finds and returns the diameter of the component entered. If there are more than one component present, this will return an error message."""
+
+    _module_type_name = "calculate.average_path"
+
+    def create_inputs_schema(self):
+        return {
+            "network_component": {
+                "type": "network_graph",
+                "doc": "The network component being queried.",
+            }
+        }
+
+    def create_outputs_schema(self):
+        return {
+            "diameter": {
+                "type": "string",
+                "doc": "The length of the diameter for the component.",
+            },
+        }
+    
+    def process(self, inputs: ValueMap, outputs: ValueMap):
+
+        import networkx as nx
+        from kiara_plugin.tropy.models import NetworkGraph
+
+        edges = inputs.get_value_obj("network_component")
+
+        network_data: NetworkGraph = edges.data
+
+        G = network_data.as_networkx_graph()
+        if len(sorted(nx.connected_components(G.to_undirected()), key=len, reverse=True)) > 1:
+            raise KiaraProcessingException(
+                    f"Graph contains more than one component. Please choose either the largest component or a graph with only one component."
+            )
+        
+        if nx.is_weighted(G, weight='weight') == True:
+            weights = True
+        else:
+            weights = False
+        
+        diameter = nx.diameter(G.to_undirected(), weight=weights)
+
+        outputs.set_values(diameter=diameter)
